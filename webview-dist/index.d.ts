8a6025dd8ba974a536a9c4c0307a29370c8dde87
Baginda Namora <13404381+bagindo@users.noreply.github.com>
2023-11-08 21:50:17 +0800

    build - js api

--- webview-dist/index.d.ts
+++ webview-dist/index.d.ts
@@ -1,2 +1,23 @@
-export declare function execute(): Promise<unknown>;
-export declare function validateLicense(licenseKey: string): Promise<unknown>;
+export declare type KeygenLicense = {
+    key: string;
+    code: string;
+    detail: string;
+    expiry: string;
+    valid: string;
+    policyId: string;
+};
+export declare type KeygenError = {
+    code: string;
+    detail: string;
+};
+export declare function isLicensed(): Promise<boolean>;
+export declare function getLicense(): Promise<KeygenLicense | null>;
+export declare function getLicenseKey(): Promise<string | null>;
+export declare function canUpdate(): Promise<boolean>;
+export declare function validateLicense({ key, cacheResponse, }: {
+    key: string;
+    cacheResponse?: boolean;
+}): Promise<KeygenLicense>;
+export declare function activateMachine(): Promise<KeygenLicense>;
+export declare function licenseNotActivated(license: KeygenLicense): boolean;
+export declare function checkoutMachine(): Promise<void>;
